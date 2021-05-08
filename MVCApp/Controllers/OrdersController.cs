84e1421da30e9aa5bfb29aafc7bd9ab843f225e8
erkanisuf <63913743+erkanisuf@users.noreply.github.com>
2021-05-09 01:48:35 +0300

    tbl

---
+++ MVCApp/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Data;
+using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+
+        private readonly NorthwindDB _context;
+
+        public OrdersController(NorthwindDB context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            List<Order> orders = _context.Orders.ToList();
+            List<OrderDetail> orderdetails = _context.OrderDetails.ToList();
+            List<Customer> customer = _context.Customers.ToList();
+            var innerjoin = from a in orders
+                            join b in orderdetails on a.OrderId equals b.OrderId 
+                            join c in customer on a.CustomerId equals c.CustomerId
+                            select new OrdersJoined{ order = a, orderdetail=b ,customer=c};
+            return View(innerjoin.ToList());
+        }
+    }
+}
---
+++ MVCApp/Models/OrdersJoined.cs
@@ -0,0 +1,15 @@
+﻿using MVCApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Models
+{
+    public class OrdersJoined
+    {
+        public Order order { get; set; }
+        public OrderDetail orderdetail { get; set; }
+        public Customer customer { get; set; }
+    }
+}
