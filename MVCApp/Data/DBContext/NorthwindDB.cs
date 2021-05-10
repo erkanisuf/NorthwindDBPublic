30f57f413a42e6d11883f4b27331cf2bbb447284
erkanisuf <63913743+erkanisuf@users.noreply.github.com>
2021-05-10 23:14:54 +0300

    search filter

--- MVCApp/Data/DBContext/NorthwindDB.cs
+++ MVCApp/Data/DBContext/NorthwindDB.cs
@@ -58,8 +58,7 @@ namespace MVCApp.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-FA527TG;Initial Catalog=northwindtest;Persist Security Info=True;User ID=sa;Password=Need2260");
+            optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("DB"));
             }
         }
 
